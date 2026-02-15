fcb614f7452b2f5a361f61643b53c4669398464e
W1sCont <tmrostik@gmail.com>
2026-02-15 12:25:53 +0000

    lesson 12 tasks correct

--- lesson12task.cpp
+++ lesson12task.cpp
@@ -2,6 +2,7 @@
 #include <string>
 
 using std::cout;
+using std::endl;
 using std::string;
 
 
@@ -11,97 +12,98 @@ protected:
     int diameter;
 public:
     explicit Bolt(int d = 8) : diameter{ d } { cout << "Bolt constructor\n"; }
-    virtual ~Bolt() { cout << "Bolt destructor\n"; }
-    int getDiameter() const { return diameter; }
-    void setDiameter(int d) { diameter = d; }
-    void print() const { cout << "Bolt diameter: " << diameter << "\n\n"; }
+    ~Bolt() { cout << "Bolt destructor\n"; }
+    virtual int getDiameter() const { return diameter; }
+    virtual void print() const {
+        cout << "Bolt diameter: " << diameter << "\n\n";
+    }
 };
 
 
 // ...  Wheels, Engine, Doors ...+-
-class Wheels {
+class Wheels : virtual public Bolt{
 protected:
     int radius;
-    Bolt bolt;
 public:
-    Wheels() : radius{ 19 }, bolt{ 12 } { cout << "Wheels constructor\n"; }
-    explicit Wheels(int radius_) : radius{ radius_ }, bolt{ 15 } { cout << "Wheels constructor\n"; }
-    virtual ~Wheels() {cout << "Wheels destructor\n";}
+    Wheels() = delete;
+    Wheels(int radius_, int b) : radius{ radius_ }, Bolt{ b } { cout << "Wheels constructor\n"; }
+    ~Wheels() {cout << "Wheels destructor\n";}
 
     int getRadius() const { return radius; }
 
     virtual void print() const {
         cout << "Wheels radius: " << radius << '\n';
-        bolt.print();
+        Bolt::print();
     }
 };
 
-class Engine {
+class Engine : virtual public Bolt{
 protected:
     int power;
-    Bolt bolt;
 public:
-    Engine() : power{ 150 } { cout << "Engin constructor\n"; } // 150 HP = 111kW
-    explicit Engine(int power_) : power{ power_ }, bolt{ 14 }  { cout << "Engin constructor\n"; }
-    virtual ~Engine() { cout << "Engin destructor\n"; }
+    Engine() = delete;
+    Engine(int power_, int b) : power{ power_ }, Bolt{ b } { cout << "Engin constructor\n"; }
+    ~Engine() { cout << "Engin destructor\n"; }
 
     int getPower() const { return power; }
-    void setBoltSize(int d) { bolt.setDiameter(d); }
 
     virtual void print() const {
         cout << "Engine power: " << power << '\n';
-        bolt.print();
+        Bolt::print();
     }
 };
 
-class Doors {
+class Doors : virtual public Bolt {
 protected:
     string side;
-    Bolt bolt;
 public:
-    Doors() : side {"1right 1left" } { cout << "Doors constructor\n"; }
-    explicit Doors(string side_) : side{ side_ }, bolt{ 13 }  { cout << "Doors constructor\n"; }
-    virtual ~Doors() { cout << "Doors destructor\n"; }
+    Doors() = delete;
+    Doors(string side_, int b) : side{ side_ }, Bolt{ b }  { cout << "Doors constructor\n"; }
+    ~Doors() { cout << "Doors destructor\n"; }
 
     string getSide() const { return side; }
-    void setBoltSize(int d) { bolt.setDiameter(d); }
+
     virtual void print() const {
         cout << "Doors side: " << side << '\n';
-        bolt.print();
+        Bolt::print();
     }
 };
 
-class Car : public Bolt, public Wheels, public Engine, public Doors
+class Car :  public Wheels, public Engine, public Doors
 {
 protected:
     string type;
-    Bolt bolt;
 public:
-    Car() : type{ "diesel" } { cout << "Car constructor\n"; }
-    Car(string type_, int radius_, int power_, string side_ )
-        : type{ type_ }, Wheels{ radius_ }, Engine(power_), Doors{ side_ }, bolt{ 12 } { cout << "Car constructor\n"; }
-    ~Car() override { cout << "Car destructor\n"; }
+    Car() : type{ "diesel" }, Wheels{17, 0}, Engine{152, 0}, Doors{"1right 1 left", 0}, Bolt{ 16 } { cout << "Car constructor\n"; }
+    Car(string type_, int radius_, int power_, string side_, int b )
+        : type{ type_ }, Wheels{ radius_, 0 }, Engine(power_, 0), Doors{ side_, 0 }, Bolt{ b } { cout << "Car constructor\n"; }
+    ~Car() { cout << "Car destructor\n"; }
 
     string getType() const { return type; }
+    int getDiameter() const override { return diameter; }
 
 
     void print() const override {
         cout << "\n--- Full Car Info (" << type << ") ---\n";
+        cout << "Wheels: ";
         Wheels::print();
+        cout << "Engine: ";
         Engine::print();
+        cout << "Doors: ";
         Doors::print();
-        cout << "Car own mounting: ";
-        bolt.print();
+        cout << "Car own mounting:\n";
+        Bolt::print();
     }
+
 };
 
 
 int main()
 {
-    Bolt b(10);		// test
-    cout << "Bolt " << b.getDiameter() << "\n\n";
+    // Bolt b(10);		// test
+    // cout << "Bolt " << b.getDiameter() << "\n\n";
 
-    Car tesla{"electro", 22, 155, "2left 2right", };
+    Car tesla{"electro", 22, 155, "2left 2right", 23 };
     cout << "\n============\n";
     cout << "My car tesla: \n";
     tesla.print();
@@ -110,6 +112,7 @@ int main()
     cout << "\n============\n";
     cout << "My car ww passat: \n";
     passat.print();
+
 
     return 0;
 }
